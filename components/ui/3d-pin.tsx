51a7b77ac24a1475ac9b388f7216d4294fb74a42
JaeWon Jin <j1jin286@gmail.com>
2025-02-19 15:49:47 -0800

    Fixed anchor tags for project links

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -41,15 +41,16 @@ export const PinContainer = ({
   const onMouseLeave = () => {
     setTransform("translate(-50%,-50%) rotateX(0deg) scale(1)");
   };
-
+console.log("Link", href)
   return (
-    <div
+    <a
       className={cn(
         "relative group/pin z-50  cursor-pointer",
         containerClassName
       )}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
+      href={href}
     >
       <div
         style={{
@@ -68,7 +69,7 @@ export const PinContainer = ({
         </div>
       </div>
       <PinPerspective title={title} href={href} />
-    </div>
+    </a>
   );
 };
 
@@ -91,13 +92,14 @@ export const PinPerspective = ({
   title?: string;
   href?: string;
 }) => {
+  console.log("Pin href", href)
   return (
     <motion.div className="pointer-events-none  w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
-          <a
-            href={href || "/"}
-            target={"_blank"}
+          <div
+            // href={href || "/"}
+            // target={"_blank"}
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
           >
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
@@ -105,7 +107,7 @@ export const PinPerspective = ({
             </span>
 
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
+          </div>
         </div>
 
         <div
